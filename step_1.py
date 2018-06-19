612a07e994211f4472b493b03dd31a172e2f2528
Ravi S <rsavaj@gmail.com>
2018-06-19 03:12:24 +0000

    group products by customer and invoice

--- step_1.py
+++ step_1.py
@@ -1,2 +1,10 @@
+from data import products_string
+
 def transform_products_to_list(products_string):
-    pass
+    split_products = products_string.split('\n')
+    products_list = []
+    for product_line in split_products:
+        if product_line:
+            product = product_line.split(',')
+            products_list.append(product)
+    return products_list
--- step_3.py
+++ step_3.py
@@ -1,5 +1,33 @@
 from step_1 import transform_products_to_list
-
+from data import products_string
+from pprint import pprint
 
 def group_products_by_customer_and_invoice(products_string):
-    pass
+    products_list = transform_products_to_list(products_string)
+    
+    products = {}
+    
+    for product in products_list:
+        customer_id = product[-2]
+        invoice_id = product[0]
+        
+        products.setdefault(customer_id, {})
+        products[customer_id].setdefault(invoice_id, [])
+        products[customer_id][invoice_id].append(product)
+        
+    return products
+
+products = group_products_by_customer_and_invoice(products_string)
+pprint(products)
+print('*******************************')
+customer = products['12583']
+# print(customer)
+invoice = customer['536370']
+# print(invoice)
+row_item = invoice[1]
+# print(row_item)
+description = row_item[2]
+print(description)
+
+result = products['12583']['536370'][1][2]
+print(result)
